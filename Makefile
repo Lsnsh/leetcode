908b9303c7da0bebcb311af296239330104da10f
Lsnsh <zehexin6@gmail.com>
2020-06-30 17:15:51 +0800

    feat: (script) support to use make command create problem solution directory structure

---
+++ Makefile
@@ -0,0 +1,9 @@
+# .PHONY: dist test
+default: help
+
+new:
+	node build/bin/new.js $(filter-out $@,$(MAKECMDGOALS))
+
+help:
+	@echo "   \033[35mmake\033[0m \033[1m命令使用说明\033[0m"
+	@echo "   \033[35mmake new <problem-number> <problem-name> [中文名]\033[0m\t---  添加新的 leetcode 题目 例如 'make new 1 two-sum 两数之和'"
---
+++ build/bin/new.js
@@ -0,0 +1,56 @@
+'use strict';
+
+console.log();
+process.on('exit', () => {
+  console.log();
+});
+
+if (!process.argv[2]) {
+  console.error('[题目编号]必填 - Please enter new problem number');
+  process.exit(1);
+}
+if (!process.argv[3]) {
+  console.error('[题目名称]必填 - Please enter new problem name');
+  process.exit(1);
+}
+
+const path = require('path');
+const fileSave = require('file-save');
+const problemNumber = process.argv[2];
+const problemName = process.argv[3];
+const chineseName = process.argv[4] || problemName;
+const problemParentFolderName = problemNumber % 100 === 0 ? `${problemNumber - 99}-${problemNumber}` : `${Math.floor(problemNumber / 100) * 100 + 1}-${Math.ceil(problemNumber / 100) * 100}`
+const problemFolderPath = path.resolve(__dirname, '../../src', problemParentFolderName, `${problemNumber}.${problemName}`);
+const Files = [
+  {
+    filename: 'index.js',
+    content: `// ${chineseName}`
+  },
+  {
+    filename: 'README.md',
+    content: `## ${chineseName}`
+  },
+];
+
+// 添加到 dir.json
+const dirFile = require('../../dir.json');
+if (!dirFile[problemParentFolderName]) {
+  dirFile[problemParentFolderName] = {};
+}
+if (dirFile[problemParentFolderName] && dirFile[problemParentFolderName][`${problemNumber}.${problemName}`]) {
+  console.error(`${problemFolderPath} 已存在.`);
+  process.exit(1);
+}
+dirFile[problemParentFolderName][`${problemNumber}.${problemName}`] = `${problemNumber}. ${chineseName}`;
+fileSave(path.join(__dirname, '../../dir.json'))
+  .write(JSON.stringify(dirFile, null, '  '), 'utf8')
+  .end('\n');
+
+// 新建文件
+Files.forEach(file => {
+  fileSave(path.join(problemFolderPath, file.filename))
+    .write(file.content, 'utf8')
+    .end('\n');
+});
+
+console.log('DONE!');
